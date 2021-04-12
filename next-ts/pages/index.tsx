ab831bafdfff5cfa5f34d717af1a8b6a1ecac551
InJaEE <in11202@naver.com>
2021-04-13 00:08:25 +0900

    set up next-ts todo

---
+++ next-ts/pages/index.tsx
@@ -0,0 +1,16 @@
+import { NextPage } from 'next';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    padding: 20px;
+`;
+
+const index: NextPage = () => {
+    return (
+    <Container>
+        <h1>EE</h1>
+    </Container>
+    )
+}
+
+export default index;
